9de55a7257d65abee645533c337ffec2a0c4be58
CSCMWS\wnunes2 <wilian.bnunes@gmail.com>
2020-12-23 17:52:31 -0300

    Novas condicoes

--- 02_C_scripts/resolv_DNS.c
+++ 02_C_scripts/resolv_DNS.c
@@ -4,13 +4,18 @@
 
 int main (int argc, char *argv[])
 {
-	char *alvo;
-	alvo = argv[1];
-
-	struct hostent *host;
-
-	host=gethostbyname(alvo);
-
-	printf("%s ===> %s \n",alvo, inet_ntoa(*((struct in_addr *)host->h_addr)));//transforma em endereco IP
-	return 0;
+	if (argc <= 1){
+		printf("Modo de uso: ./resolv_DNS www.businesscorp.com.br\n");
+	} else{
+			char *alvo;
+			alvo = argv[1];
+			struct hostent *host;
+			host=gethostbyname(alvo);
+		if (alvo == NULL){
+			printf("Ocorreu um erro :( \n");
+		}	else{
+				printf("%s ===> %s \n",alvo, inet_ntoa(*((struct in_addr *)host->h_addr)));//transforma em endereco IP
+				return 0;
+				}
+		}
 }
